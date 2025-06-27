756cb0f8b04f28d0c4008d70ebfe750177c84686
Kyle Cannoles <kylecann@hawaii.edu>
2025-06-27 14:52:55 +0900

    Update bingo template and add features

--- main.py
+++ main.py
@@ -1,42 +1,80 @@
 import random
+import os
 
-images = [
-    "bag",
-    "bat",
-    "bed",
-    "bicycle",
-    "board_game",
-    "book",
-    "video_games",
-    "hat",
-    "piano",
-    "pillow",
-    "plushie",
-    "racket",
-    "shoes",
-    "soccer_ball",
-    "sweater",
-    "Nintendo_Switch",
-    "toys",
-    "trampoline",
-    "wallet",
-    "watch"
-]
+#settings
+num_squares = 16
+squares_per_line = 4
+num_pages = 50
+project_name = "schedule_bingo"
+title = "My Schedule Bingo"
+dialogA = "What time do you get up?"
+underline = ["get up", "6:00", "clean my room", "clean your room", "4:00"]
+dialogB = "I get up at 6:00." 
+bold = ["my", "your"]
+dialogA2 = "What time do you clean your room?"
+dialogB2 = "I clean my room at 4:00."
+file_name = project_name
+graphics_path_list = ["images/","images/characters"]
 
-# settings
+word_list = True
+
+
+for s in underline:
+    dialogA = dialogA.replace(s, "\\underline{"+s+"}")
+    dialogB = dialogB.replace(s, "\\underline{"+s+"}")
+
+for s in underline:
+    dialogA2 = dialogA2.replace(s, "\\underline{"+s+"}")
+    dialogB2 = dialogB2.replace(s, "\\underline{"+s+"}")
+
+for s in bold:
+    dialogA2 = dialogA2.replace(s, "\\textbf{"+s+"}")
+    dialogB2 = dialogB2.replace(s, "\\textbf{"+s+"}")
+
+graphics_path_string = ""
+for path in graphics_path_list:
+    graphics_path_string += "{" + path + "}"
+
+images = [os.path.splitext(f)[0] for f in os.listdir(f"./{project_name}/images") if f.endswith((".png", ".jpg", ".jpeg", ".gif"))]
+
+word_table = r"""
+\medskip
+\def\arraystretch{1.5}%  1 is the default, change whatever you need
+\begin{footnotesize}
+\begin{tabular}{ccccc}
+"""
+
+for i,img in enumerate(sorted(images)):
+    word_table += img.replace("_", " ")
+    if i < len(images) and i % 5 != 4:
+        word_table += " & "
+    else:
+        word_table += r" \\" + "\n"
+
+word_table += r"""\end{tabular}
+\end{footnotesize}
+\medskip
+"""
+
 setup = r"""\documentclass[12pt, a4paper]{article}
 \usepackage[
 a4paper, 
 margin=1cm,
 includehead,
-headheight=16pt,
+headheight=32pt,
 nomarginpar,% We don't want any margin paragraphs
 ]{geometry}
 \usepackage{fancyhdr} % headers
 \usepackage[normalem]{ulem} % underlining
 
 \usepackage{graphicx} % Required for inserting images
-\graphicspath{{images/}}
+\graphicspath{""" + graphics_path_string + r"""}
+
+\usepackage{tikz}
+\usetikzlibrary{positioning,shapes.callouts} % for speech bubbles
+
+\usepackage{array} % for '\newcolumntype' macro
+\usepackage{verbatimbox}
 
 \usepackage{fontspec} % custom fonts
 \setmainfont{MitsuEHandwriting}[
@@ -47,14 +85,25 @@ nomarginpar,% We don't want any margin paragraphs
     ItalicFont=*Italic-R,
     BoldItalicFont=*Italic-B
     ]
+\setmonofont{MitsuEHandwriting4lines}[
+    Path=../MitsuEHandwriting/,
+    Extension = .otf,
+    UprightFont=*-R,
+    BoldFont=*-B,
+    ItalicFont=*Italic-R,
+    BoldItalicFont=*Italic-B
+    ]
+
+\newcolumntype{C}[1]{>{\centering\arraybackslash}m{#1}} % centered version of 'm' col. type
 
 \begin{document}
 % Set the page style to "fancy"...
 \pagestyle{fancy}
 \fancyhf{} % clear existing header/footer entries
+\fancyhfinit{\bfseries\ttfamily}
 % We don't need to specify the O coordinate
-\fancyhead[L]{Name:\uline{\hspace{13em}}}
-\fancyhead[R]{Class:\uline{\hspace{2em}--\hspace{2em}}\hspace{2em} Number:\uline{\hspace{3em}}\hspace{2em}Date:  \uline{\hspace{2em}/\hspace{2em}}}
+\fancyhead[L]{\Huge{Name:||||||||||||||}}
+\fancyhead[R]{\Huge{Date:|||||||}}
 \renewcommand{\headrulewidth}{0pt}
 """
 
@@ -64,57 +113,76 @@ end = r"""
 page_start = r"""
 \centering
 \begin{Huge}
-\textbf{Birthday Present Bingo}
+\textbf{""" + f"{title}" + r"""}
 \end{Huge}%
+\medskip
 
-\def\arraystretch{2}
-\begin{tabular}{l}
-    A: What do you want for your birthday? \\
-    B: I want \underline{a new bicycle}. \\
-\end{tabular}
+\begin{tikzpicture}
+  \node at (-9, 0) (charA) {\includegraphics[width=3cm,height=3cm]{charA}};
+  \node [inner sep=8pt, thick, draw, align=center,
+         rectangle callout, rounded corners, callout relative pointer={(-1.5,-1.0)},
+         above right = 0 and -2 cm of charA.north east]
+    {""" + f"{dialogA}" + r"""};
+  \node [inner sep=8pt, thick, draw, align=center,
+         rectangle callout, rounded corners, callout relative pointer={(-1,0)},
+         above right = -2 and 0 cm of charA.north east]
+    {""" + f"{dialogA2}" + r"""};
+  \node at (6, 0) (charB) {\includegraphics[width=3cm,height=3cm]{charB}};
+  \node [inner sep=8pt, thick, draw, align=center,
+         rectangle callout, rounded corners, callout relative pointer={(2,-1)},
+         above left = -0.5 and 0 cm of charB.north west]
+    {""" + f"{dialogB}" + r"""};
+  \node [inner sep=8pt, thick, draw, align=center,
+         rectangle callout, rounded corners, callout relative pointer={(2,0.5)},
+         above left = -3.5 and 0 cm of charB.north west]
+    {""" + f"{dialogB2}" + r"""};
+\end{tikzpicture}
 
-\begin{center}
 """
 page_begin_table = r"""
-\def\arraystretch{1.5}
-\begin{tabular}{|c|c|c|c|}
+\def\arraystretch{1.5}%  1 is the default, change whatever you need
+\medskip
+\begin{tabular}{| C{4cm} | C{4cm} | C{4cm}| C{4cm}| }
 \hline
 """
 page_end_table = r"""
 \end{tabular}
-\end{center}
 """
 
-num_squares = 16
-squares_per_line = 4
-num_pages = 35
-file_name = "birthday_gifts_bingo"
-with open(f"./{file_name}//{file_name}"+".tex", mode='w') as f:
+with open(f"./{project_name}//{file_name}_p{num_pages}"+".tex", mode='w') as f:
     f.write(setup)
     for pg_num in range(1,num_pages+1):
         f.write(f"% pg {pg_num} start\n")
         f.write(page_start)
-        f.write(page_begin_table)
+        if word_list:
+            f.write(word_table)
 
+        f.write(page_begin_table)
         random.shuffle(images)
         img_names = ""
         imgs = ""
+        line_text = ""
         for i,img in enumerate(images):
             if i > num_squares-1:
                 break
+            line_text += img.replace("_", " ")
+            line_text += " \\strut\n\n"
+            line_text += "\\includegraphics[width=3.5cm,height=2.8cm,keepaspectratio]{"+f"{img}"+"}"
+
             img_names += img.replace("_", " ")
-            imgs += "\\includegraphics[width=4cm,height=4cm,keepaspectratio]{"+f"{img}"+"}"
-            #f.write("\\includegraphics[width=4cm,height=4cm,keepaspectratio]{"+f"{img}"+"}")
+            imgs += "\\includegraphics[width=3cm,height=3cm,keepaspectratio]{"+f"{img}"+"}"
             if i % squares_per_line != squares_per_line-1:
+                line_text += " & "
                 img_names += " & "
                 imgs += " & "
-                #f.write(" & ")
             else:
-                f.write(imgs)
+                f.write(line_text)
+                #f.write(imgs)
                 f.write(" \\\\\n")
-                f.write(img_names)
-                f.write(" \\\\\n")
+                #f.write(img_names)
+                #f.write(" \\\\\n")
                 f.write("\\hline\n")
+                line_text = ""
                 img_names = ""
                 imgs = ""
             i += 1
